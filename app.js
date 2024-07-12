e07414419b131cf81cbbaf1797a9f4fe150ed7c6
Santhosh174 <santhoshcse18@gmail.com>
2024-07-12 20:49:24 +0530

    using nodejs

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const route = require('./routes/route')
+
+app.listen(port, () => {
+    console.log(`App is running on localhost:${port}`);
+});
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+app.use(route)
+
+app.get('/home',(req,res)=>{
+    res.redirect('/');
+})
---
+++ routes/route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const route = express.Router();
+const controller = require('../controller/controller')
+
+route.get('/',controller.index);
+
+module.exports = route;
